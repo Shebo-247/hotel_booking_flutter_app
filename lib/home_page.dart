0d2abd12a1dba0091e4eecbc38cfa64751a4c02e
shebo-247 <shouaibmoha247@gmail.com>
2020-05-07 00:29:39 +0200

    adding list view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -56,7 +56,9 @@ class _HomePageState extends State<HomePage> {
                               borderRadius: BorderRadius.circular(20))),
                     ),
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Container(
                     height: 60,
                     width: 60,
@@ -66,10 +68,27 @@ class _HomePageState extends State<HomePage> {
                     ),
                     child: FlatButton(
                       onPressed: () {},
-                      child: Icon(Icons.tune, color: Colors.white, size: 30,),
+                      child: Icon(
+                        Icons.tune,
+                        color: Colors.white,
+                        size: 30,
+                      ),
                     ),
                   )
                 ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 300,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    createRoom(),
+                    createRoom(),
+                  ],
+                ),
               )
             ],
           ),
@@ -77,4 +96,89 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget createRoom(){
+    return Container(
+      width: 190,
+      margin: EdgeInsets.only(right: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 220,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/room1.png'),
+                  fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_border,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Bedroom in Luxory Home',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                '\$170/night',
+                style: TextStyle(fontSize: 14),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[200]),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[900],
+                      size: 18,
+                    ),
+                    SizedBox(width: 5,),
+                    Text(
+                      '5.0',
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
 }
